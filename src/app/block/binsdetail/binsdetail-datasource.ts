55e8fa1171a12475c9b1cbbe555850348ea3e7b4
DIVYA PRAKASH <medivyaprakash@gmail.com>
2020-01-19 03:54:38 +0530

    update

--- src/app/block/binsdetail/binsdetail-datasource.ts
+++ src/app/block/binsdetail/binsdetail-datasource.ts
@@ -7,19 +7,31 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 // TODO: Replace this with your own data model type
 export interface BinsdetailItem {
   name: string;
-  schallot: string;
-  sex: string;
-  qual: string;
-  email: string;
-  adata: string;
-  fund: number;
-  dist: string;
-  block: string;
+  id: number;
 }
 
 // TODO: replace this with real data from your application
-export var EXAMPLE_DATA: BinsdetailItem[] = [
-  
+const EXAMPLE_DATA: BinsdetailItem[] = [
+  {id: 1, name: 'Hydrogen'},
+  {id: 2, name: 'Helium'},
+  {id: 3, name: 'Lithium'},
+  {id: 4, name: 'Beryllium'},
+  {id: 5, name: 'Boron'},
+  {id: 6, name: 'Carbon'},
+  {id: 7, name: 'Nitrogen'},
+  {id: 8, name: 'Oxygen'},
+  {id: 9, name: 'Fluorine'},
+  {id: 10, name: 'Neon'},
+  {id: 11, name: 'Sodium'},
+  {id: 12, name: 'Magnesium'},
+  {id: 13, name: 'Aluminum'},
+  {id: 14, name: 'Silicon'},
+  {id: 15, name: 'Phosphorus'},
+  {id: 16, name: 'Sulfur'},
+  {id: 17, name: 'Chlorine'},
+  {id: 18, name: 'Argon'},
+  {id: 19, name: 'Potassium'},
+  {id: 20, name: 'Calcium'},
 ];
 
 /**
@@ -83,15 +95,7 @@ export class BinsdetailDataSource extends DataSource<BinsdetailItem> {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
         case 'name': return compare(a.name, b.name, isAsc);
-        case 'schallot': return compare(a.schallot, b.schallot, isAsc);
-        case 'sex': return compare(a.sex, b.sex, isAsc);
-        case 'qual': return compare(a.qual, b.qual, isAsc);
-        case 'email': return compare(a.email, b.email, isAsc);
-        case 'adata': return compare(a.adata, b.adata, isAsc);
-        case 'fund': return compare(a.fund, b.fund,isAsc);
-        case 'dist': return compare(a.dist, b.dist,isAsc);
-        case 'block': return compare(a.block, b.block, isAsc);
-        
+        case 'id': return compare(+a.id, +b.id, isAsc);
         default: return 0;
       }
     });
